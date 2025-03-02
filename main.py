bae0e327a4d39729c4c0eeb5e1d28150c19b3c74
Toletum <toletum@gmail.com>
2025-03-02 13:47:20 +0100

    Refactor

--- main.py
+++ main.py
@@ -29,30 +29,40 @@ client = Client(host=host)
 
 ini = time.time()
 
-response = client.chat(model=model,
-                       stream=False,
-                       format=Query.model_json_schema(),
-                       messages=[
+messages = [
     {
         'role': 'system',
-        'content':system_content
+        'content': system_content
     },
     {
         'role': 'user',
         'content': "query with all clients from city is 'CiudadA'",
-    },
-])
+    } ]
+
+response = client.chat(model=model,
+                       stream=False,
+                       format=Query.model_json_schema(),
+                       messages=messages)
 
 end = time.time()
 
-query = Query.model_validate_json(response.message.content)
-print(json.dumps(query.model_dump(), indent=2))
+try:
+    query = Query.model_validate_json(response.message.content)
+    print(json.dumps(query.model_dump(), indent=2))
+except Exception as ex:
+    print("Error JSON to CLASS")
+    exit(1)
 
-db = sqlite3.connect(query.database)
-cursor = db.cursor()
-cursor.execute(query.sql)
-for fila in cursor.fetchall():
-    print(fila)
-db.close()
+try:
+    db = sqlite3.connect(query.database)
+    cursor = db.cursor()
+    cursor.execute(query.sql)
+    for fila in cursor.fetchall():
+        print(fila)
+    db.close()
+except Exception as ex:
+    print(ex)
+    print("SQL in Sqlite")
+    exit(1)
 
 print(f" total:  {(end - ini)}")
